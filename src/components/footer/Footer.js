f98337018a5be2553c1e01712d97a7d9ccf40fed
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-26 20:39:53 -0300

    Footer iniciado.

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Footer } from "./styled";
+
+const Header = () => {
+  return (
+    <div>
+      <Footer>© Copyright 2021 - Todos os direitos reservados.</Footer>
+    </div>
+  );
+};
+
+export default Header;
